12714ac60fdb75bf71fffb01fa29e1bc54cd696d
kazmy <nehaluddin.dpe@gmail.com>
2023-04-20 21:47:40 +0530

    completed my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
 const myUnderscore = require('underscore')
 
 router.get('/test-me', function (req, res) {
@@ -48,5 +47,38 @@ router.get('/students/:studentName', function(req, res) {
     res.send('student data')
 })
 
+const array = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+router.get('/movies',function(req, res){
+    console.log(array)
+})
+router.get('/movies/:index',function(req,res){
+    if(req.params.index<array.length && req.params.index>=0){
+        res.send(array[req.params.index])
+    }
+    else{
+        res.send("out Of index")
+    }
+})
+
+const arr=[ {'id': 1,'name': 'The Shining'}, 
+    {'id': 2,'name': 'Incendies'}, 
+    {'id': 3,'name': 'Rang de Basanti'},
+    {'id': 4,'name': 'Finding Nemo'}]
+
+router.get('/films',function(req,res){
+    res.send(arr)
+})
+router.get('/films/:filmid',function(req, res){
+    let flag =0
+    for(let i=0; i<arr.length; i++){
+        if(req.params.filmid==arr[i].id){
+            res.send(arr[i])
+            flag=1
+        }
+    }
+    if(flag == 0){
+        res.send("No movie exists with this id")
+    }
+})
 
 module.exports = router;
