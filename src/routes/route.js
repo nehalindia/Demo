e8f423d96341a95bb60ee562527b866586de082d
kazmy <nehaluddin.dpe@gmail.com>
2023-04-24 22:14:54 +0530

    completed array problem

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,4 +36,20 @@ router.get('/films/:filmid',function(req, res){
     }
 })
 
+router.get('/sol1',function(req, res){
+    let arr = [1,2,3,4,5,6,7,8,9,11]
+    let sum = arr.reduce((x,y) => x+y)
+    let n = arr.length + 1
+    let missingvalue = ((n*(n+1))/2) - sum
+    console.log('missingvalue :'+missingvalue)
+});
+
+router.get('/sol2',function(req, res){
+    let arr = [36,37,38,39,41,40,42,44]
+    let sum = arr.reduce((x,y) => x+y)
+    let n = arr.length +1
+    let missingvalue = ((n*(arr[0]+arr[arr.length-1]))/2) - sum
+    console.log('missingvalue :'+missingvalue)
+});
+
 module.exports = router;
