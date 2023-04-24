6ff4bed443bf89425c914163bc7ca4a96716a930
kazmy <nehaluddin.dpe@gmail.com>
2023-04-25 01:01:21 +0530

    Post method

--- src/routes/route.js
+++ src/routes/route.js
@@ -52,4 +52,51 @@ router.get('/sol2',function(req, res){
     console.log('missingvalue :'+missingvalue)
 });
 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+   router.post('/players', function(req,res){
+    let flag = 0
+    for(let i=0; i<players.length; i++){
+        if(req.body.name == players[i].name){
+            flag = 1
+        }
+    }
+    if(flag == 0){
+        players.push(req.body)
+        res.send({data : players, status : true})
+    }
+    else{
+        res.send({Response : "Data Already Exist"})
+    }
+   })
+
 module.exports = router;
