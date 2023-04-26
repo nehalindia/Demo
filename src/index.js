eed8dc7b5315ef3e14f7bd2254aeaf43aef151ce
kazmy <nehaluddin.dpe@gmail.com>
2023-04-26 22:19:23 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -13,9 +13,3 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
-
-app.listen(process.env.PORT || 5000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 5000))
-});
-
-
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,151 +1,163 @@
 const express = require('express');
-const { route } = require('express/lib/application');
 const router = express.Router();
 
-
-//movies problem from W5D4
-const array = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-router.get('/movies',function(req, res){
-    console.log(array)
-})
-router.get('/movies/:index',function(req,res){
-    if(req.params.index<array.length && req.params.index>=0){
-        res.send(array[req.params.index])
-    }
-    else{
-        res.send("out Of index")
-    }
+router.get('/students/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
 })
 
-const arr=[ {'id': 1,'name': 'The Shining'}, 
-    {'id': 2,'name': 'Incendies'}, 
-    {'id': 3,'name': 'Rang de Basanti'},
-    {'id': 4,'name': 'Finding Nemo'}]
-
-router.get('/films',function(req,res){
-    res.send(arr)
-})
-router.get('/films/:filmid',function(req, res){
-    let flag =0
-    for(let i=0; i<arr.length; i++){
-        if(req.params.filmid==arr[i].id){
-            res.send(arr[i])
-            flag=1
-        }
-    }
-    if(flag == 0){
-        res.send("No movie exists with this id")
-    }
+router.get("/random" , function(req, res) {
+    res.send("hi there")
 })
 
 
-// missing array value using get W6D1
-router.get('/sol1',function(req, res){
-    let arr = [1,2,3,4,5,6,7,8,9,11]
-    let sum = arr.reduce((x,y) => x+y)
-    let n = arr.length + 1
-    let missingvalue = ((n*(n+1))/2) - sum
-    console.log('missingvalue :'+missingvalue)
-});
-
-router.get('/sol2',function(req, res){
-    let arr = [36,37,38,39,41,40,42,44]
-    let sum = arr.reduce((x,y) => x+y)
-    let n = arr.length +1
-    let missingvalue = ((n*(arr[0]+arr[arr.length-1]))/2) - sum
-    console.log('missingvalue :'+missingvalue)
-});
+router.get("/test-api" , function(req, res) {
+    res.send("hi FunctionUp")
+})
 
 
-// players problem using post W6D1
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ]
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ]
-       }
-   ]
+router.get("/test-api-2" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API")
+})
 
-   router.post('/players', function(req,res){
-    let flag = 0
-    for(let i=0; i<players.length; i++){
-        if(req.body.name == players[i].name){
-            flag = 1
-        }
+
+router.get("/test-api-3" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+})
+
+
+router.get("/test-api-4" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+
+
+router.get("/test-api-5" , function(req, res) {
+    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+router.get("/test-api-6" , function(req, res) {
+    res.send({a:56, b: 45})
+})
+
+router.post("/test-post", function(req, res) {
+    res.send([ 23, 45 , 6])
+})
+
+
+router.post("/test-post-2", function(req, res) {
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+router.post("/test-post-3", function(req, res) {
+    // let id = req.body.user
+    // let pwd= req.body.password
+
+    // console.log( id , pwd)
+
+    console.log( req.body )
+
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+
+
+router.post("/test-post-4", function(req, res) {
+    let arr= [ 12, "functionup"]
+    let ele= req.body.element
+    arr.push(ele)
+    res.send(  { msg: arr , status: true }  )
+})
+
+
+
+
+// QUERY vs PARAMS
+// Params ( path params ):-  used to load different items/pages based on a variable value in url
+// params variable name  is not visible in  the url
+
+
+router.get("/wiki/:countryName", function(req, res) {
+    
+    // ...
+    // hey nodejs , go and get me the data for countryName
+    res.send( {data: "something data"} )
+})
+
+
+
+// router.get("/wiki/india", function(req, res) {
+//     // ...
+//     res.send( {data: "something india"} )
+// })
+
+
+// router.get("/wiki/bangladesh", function(req, res) {
+//     // ...
+//     res.send( {data: "something bangaldesh"} )
+// })
+
+
+// router.get("/wiki/nepal", function(req, res) {
+//     // ...
+//     res.send( {data: "something nepal nepal"} )
+// })
+
+
+router.get("/:productName/p/:itemId", function(req, res) {
+    let product = req.params.productName
+    let itemId= req.params.itemId
+    // ...
+    // hey nodejs, go and get me the data for hte variable ( its value) product 
+    res.send( {data: product} )
+})
+
+// QUERY params:-
+//  use case: to make filters / searches
+// variable name is visible in the url itself
+
+
+//a get request with 2 query params
+// localhost:3000/get-query-1?myCoolVar=soemthing&xyz=functionUp
+router.get("/get-query-1", function(req, res) {
+    let data = req.query
+    let var1= data.myCoolVar
+    let var2= data.xyz
+
+    console.log( data)
+
+    res.send( {data: data, status: true} )
+})
+
+// take marks in req.query in a variable named "marks" and send "pass" if marks>40 else send "fail"
+router.get( "/get-query-2", function (req, res){
+    let marks= req.query.marks
+    let result = marks >40 ? "pass" : "fail"
+    //ternary operator
+    res.send( {data: result , status: true})
+})
+
+//query params ( as well as path params ) can be sent in post request as well
+router.post( "/post-query-1", function (req, res){
+    let data= req.query
+    console.log( data)
+    res.send( {data: data , status: true})
+})
+
+//filter out all the numbers that are greater than "input" ( input is received from query params)
+let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
+
+router.post( "/post-query-2", function (req, res){
+    let input= req.query.input
+
+    // let finArr= myArr.filter ( ele => ele>input )
+    let finalArr= []
+    for( i=0 ; i<myArr.length ; i++){
+        if ( myArr[i] > input )     finalArr.push( myArr[i])
     }
-    let pla = req.body
-    if(flag == 0){
-        players.push(pla)
-        res.send({data : players, status : true})
-    }
-    else{
-        res.send({Response : "Data Already Exist"})
-    }
-   })
+    res.send( {data: finalArr , status: true})
+})
 
-
-  // voter age problem using post W6D2
-   let persons= [
-    {
-    name: "PK",
-    age: 10,
-    votingStatus: false
- },
- {
-    name: "SK",
-    age: 20,
-    votingStatus: false
- },
- {
-    name: "AA",
-    age: 70,
-    votingStatus: false
- },
- {
-    name: "SC",
-    age: 5,
-    votingStatus: false
- },
- {
-    name: "HO",
-    age: 40,
-    votingStatus: false
- }
- ]
-router.post('/voters',function(req,res){
-    let arr = []
-    for(let i=0; i<persons.length; i++){
-        if(persons[i].age >= req.query.votingAge){
-            persons[i].votingStatus = true
-            arr.push(persons[i])
-        }
-    }
-    res.send(arr)
-}) 
 
 module.exports = router;
-
-//https://github.com/nehalindia/Demo/tree/assignment/array
