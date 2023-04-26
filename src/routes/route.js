dca9e2b0462b868c2901694b753bb62395bfb46d
kazmy <nehaluddin.dpe@gmail.com>
2023-04-27 01:08:38 +0530

    Mongo Session

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,9 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser );
+router.post("/createUser", UserController.createUser )
+
 
 router.get("/getUsersData", UserController.getUsersData)
 
